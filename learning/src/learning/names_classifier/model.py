Fix loss computation

learning/src/learning/names_classifier/model.py
@@ -61,12 +61,12 @@ class NamesClassifierLSTM(nn.Module):
             input_size=input_size,
             hidden_size=hidden_size,
             batch_first=False,
-            num_layers=2,
+            num_layers=1,
             bidirectional=True,
         )
 
         # dropout: [N, H * 2] -> [N, H * 2]
-        self.dropout = nn.Dropout(p=0.5)
+        # self.dropout = nn.Dropout(p=0.1)
 
         # fc: [N, H * 2] -> [N, C]
         self.fc = nn.Linear(hidden_size * 2, output_size)
@@ -82,10 +82,10 @@ class NamesClassifierLSTM(nn.Module):
         bidirectional_hidden_state = torch.cat((hidden[-2], hidden[-1]), dim=1)
 
         # dropout_output: [N, H * 2]
-        dropout_output = self.dropout(bidirectional_hidden_state)
+        # dropout_output = self.dropout(bidirectional_hidden_state)
 
         # output: [N, C]
-        output = self.fc(dropout_output)
+        output = self.fc(bidirectional_hidden_state)
         return output
 
 
@@ -164,5 +164,5 @@ class ParallelBatchLearner(Learner):
             outputs=outputs,
             labels=labels,
             loss=batch_loss,
-            loss_scale=1,
+            loss_scale=len(batch.samples),
         )

learning/src/learning/names_generator/model.py
@@ -9,6 +9,22 @@ from learning.learner import BatchResult, Learner
 from learning.names_generator.names_generator_dataset import NameSample
 
 
+class MLP(nn.Module):
+    def __init__(self, input_size: int, output_size: int, dropout: float):
+        super().__init__()
+        self.norm = nn.LayerNorm(input_size)
+        self.fc = nn.Linear(input_size, output_size)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.norm(x)
+        x = self.fc(x)
+        x = self.gelu(x)
+        x = self.dropout(x)
+        return x
+
+
 class NamesGenerator(nn.Module):
     def __init__(self, hidden_size, num_vocab, num_classes):
         super().__init__()
@@ -17,13 +33,11 @@ class NamesGenerator(nn.Module):
         self.num_classes = num_classes
 
         # [S, C + V + H] -> [S, H]
-        self.i2h = nn.Linear(num_classes + num_vocab + hidden_size, hidden_size)
+        self.i2h = MLP(num_classes + num_vocab + hidden_size, hidden_size, 0.2)
         # [S, C + V + H] -> [S, V]
-        self.i2o = nn.Linear(num_classes + num_vocab + hidden_size, num_vocab)
+        self.i2o = MLP(num_classes + num_vocab + hidden_size, num_vocab, 0.2)
         # [S, H + V] -> [S, V]
-        self.o2o = nn.Linear(hidden_size + num_vocab, num_vocab)
-
-        self.dropout = nn.Dropout(0.1)
+        self.o2o = MLP(hidden_size + num_vocab, num_vocab, 0.2)
 
     def forward(self, category, input, hidden):
         """
@@ -53,8 +67,6 @@ class NamesGenerator(nn.Module):
         output = self.o2o(output_combined)
         # print(f"{output.shape=}")
 
-        # [N, V]
-        output = self.dropout(output)
         return output, hidden
 
     def init_hidden(self):