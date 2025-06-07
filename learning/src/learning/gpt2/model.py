653010fd1544fd485bde87f5447dab29db58cdad
Hai Mai <thanhhaipmai@gmail.com>
2025-06-07 14:45:39 -0700

    First version of loading a pretrained gpt2

---
+++ learning/src/learning/gpt2/model.py
@@ -0,0 +1,482 @@
+from dataclasses import dataclass
+from enum import StrEnum
+from typing import Self
+
+import torch
+import torch.nn.functional as F
+from torch import nn, optim
+from transformers import GPT2LMHeadModel
+
+from learning.learner import BatchResult, Learner
+from learning.shakespeare_generator.shakespeare_dataset import Sample
+
+
+def assert_shape(name: str, tensor: torch.Tensor, shape: tuple[int, ...]):
+    if tensor.shape != shape:
+        raise ValueError(f"Invalid shape: {name}={tensor.shape}, expected: {shape=}")
+
+
+class PretrainedName(StrEnum):
+    GPT2_SMALL = "openai-community/gpt2"
+    GPT2_MEDIUM = "openai-community/gpt2-medium"
+    GPT2_LARGE = "openai-community/gpt2-large"
+    GPT2_XL = "openai-community/gpt2-xl"
+
+
+@dataclass
+class PretrainedConfig:
+    num_blocks: int
+    num_heads: int
+    embedding_size: int
+    vocab_size: int
+    sequence_length: int
+
+
+# Pretrained GPT2 configs
+PRETRAINED_CONFIG = {
+    PretrainedName.GPT2_SMALL: PretrainedConfig(
+        num_blocks=12,
+        num_heads=12,
+        embedding_size=768,
+        vocab_size=50257,
+        sequence_length=1024,
+    ),
+    PretrainedName.GPT2_MEDIUM: PretrainedConfig(
+        num_blocks=24,
+        num_heads=16,
+        embedding_size=1024,
+        vocab_size=50257,
+        sequence_length=1024,
+    ),
+    PretrainedName.GPT2_LARGE: PretrainedConfig(
+        num_blocks=36,
+        num_heads=20,
+        embedding_size=1280,
+        vocab_size=50257,
+        sequence_length=1024,
+    ),
+    PretrainedName.GPT2_XL: PretrainedConfig(
+        num_blocks=48,
+        num_heads=25,
+        embedding_size=1600,
+        vocab_size=50257,
+        sequence_length=1024,
+    ),
+}
+
+
+@dataclass
+class Config:
+    batch_size: int
+    sequence_length: int
+    embedding_size: int
+    num_heads: int
+    num_blocks: int
+    epochs: int
+    dropout: float
+    learning_rate: float
+    patience: int | None
+    min_delta: float | None
+    device: torch.device
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        if config.embedding_size % config.num_heads != 0:
+            raise ValueError(
+                f"Embedding size {config.embedding_size} must be divisible by number of heads {config.num_heads}"
+            )
+        self.num_heads = config.num_heads
+        self.head_size = config.embedding_size // config.num_heads
+
+        # shape: [B, S, E] -> [B, S, E * 3]
+        self.qkv_fc = nn.Linear(
+            config.embedding_size, config.embedding_size * 3, device=config.device
+        )
+
+        self.projection = nn.Linear(
+            config.embedding_size,
+            config.embedding_size,
+            device=config.device,
+        )
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        x: [B, S, E]
+        return: [B, S, E]
+        """
+        B, S, E = x.shape
+        assert_shape("x", x, (B, S, E))
+
+        # shape: [B, S, E]
+        q, k, v = self.qkv_fc(x).split(E, dim=-1)
+        assert_shape("q", q, (B, S, E))
+        assert_shape("k", k, (B, S, E))
+        assert_shape("v", v, (B, S, E))
+
+        # shape: [B, H, S, E // H]
+        q = q.view(B, S, self.num_heads, self.head_size).transpose(1, 2)
+        assert_shape("q", q, (B, self.num_heads, S, self.head_size))
+
+        # shape: [B, H, S, E // H]
+        k = k.view(B, S, self.num_heads, self.head_size).transpose(1, 2)
+        assert_shape("k", k, (B, self.num_heads, S, self.head_size))
+
+        # shape: [B, H, S, E // H]
+        v = v.view(B, S, self.num_heads, self.head_size).transpose(1, 2)
+        assert_shape("v", v, (B, self.num_heads, S, self.head_size))
+
+        # shape: [B, H, S, E // H]
+        output = F.scaled_dot_product_attention(
+            q,
+            k,
+            v,
+            is_causal=True,
+        )
+        assert_shape("output", output, (B, self.num_heads, S, self.head_size))
+
+        output = output.transpose(1, 2).contiguous().view(B, S, E)
+        assert_shape("output", output, (B, S, E))
+
+        output = self.projection(output)
+        assert_shape("output", output, (B, S, E))
+
+        output = self.dropout(output)
+        assert_shape("output", output, (B, S, E))
+
+        return output
+
+
+class FeedForward(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.linear = nn.Linear(
+            config.embedding_size,
+            config.embedding_size * 2,
+            device=config.device,
+        )
+        self.gelu = nn.GELU()
+        self.projection = nn.Linear(
+            config.embedding_size * 2,
+            config.embedding_size,
+            device=config.device,
+        )
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        x: [B, S, E]
+        return: [B, S, E]
+        """
+        B, S, E = x.shape
+        assert_shape("x", x, (B, S, E))
+
+        output = self.linear(x)
+        assert_shape("output", output, (B, S, E * 2))
+
+        output = self.gelu(output)
+        assert_shape("output", output, (B, S, E * 2))
+
+        output = self.projection(output)
+        assert_shape("output", output, (B, S, E))
+
+        output = self.dropout(output)
+        assert_shape("output", output, (B, S, E))
+
+        return output
+
+
+class Block(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.layer_norm1 = nn.LayerNorm(config.embedding_size, device=config.device)
+        self.heads = MultiHeadAttention(config)
+
+        self.layer_norm2 = nn.LayerNorm(config.embedding_size, device=config.device)
+        self.feed_forward = FeedForward(config)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        x: [B, S, E]
+        return: [B, S, E]
+        """
+        B, S, E = x.shape
+        assert_shape("x", x, (B, S, E))
+
+        # shape: [B, S, E]
+        output = x + self.heads(self.layer_norm1(x))
+        assert_shape("output", output, (B, S, E))
+
+        # shape: [B, S, E]
+        output = x + self.feed_forward(self.layer_norm2(output))
+        assert_shape("output", output, (B, S, E))
+
+        return output
+
+
+class GPT2(nn.Module):
+    def __init__(
+        self,
+        config: Config,
+        vocab_size: int,
+    ):
+        super().__init__()
+        self.sequence_length = config.sequence_length
+        self.vocab_size = vocab_size
+        self.embedding_size = config.embedding_size
+
+        # [B, S] of vocab index -> [B, S, E]
+        self.embedding = nn.Embedding(
+            num_embeddings=vocab_size,
+            embedding_dim=config.embedding_size,
+            device=config.device,
+        )
+
+        # [B, S] of positional index -> [B, S, E]
+        self.positional_embedding = nn.Embedding(
+            num_embeddings=config.sequence_length,
+            embedding_dim=config.embedding_size,
+            device=config.device,
+        )
+
+        # [B, S, E] -> [B, S, E]
+        self.blocks = nn.ModuleList(
+            [Block(config) for _ in range(config.num_blocks)],
+        )
+
+        self.layer_norm = nn.LayerNorm(config.embedding_size, device=config.device)
+
+        # [B, S, E] -> [B, S, V]
+        # NOTE: GPT2 uses bias=False for this last linear layer
+        self.linear = nn.Linear(
+            config.embedding_size,
+            vocab_size,
+            bias=False,
+            device=config.device,
+        )
+
+        self.positional_indices: torch.Tensor
+        self.register_buffer(
+            "positional_indices",
+            torch.arange(
+                config.sequence_length,
+                device=config.device,
+            ),
+        )
+
+    @classmethod
+    def from_pretrained(cls, pretrained_name: PretrainedName) -> Self:
+        pretrained_config = PRETRAINED_CONFIG[pretrained_name]
+
+        model = cls(
+            config=Config(
+                embedding_size=pretrained_config.embedding_size,
+                num_blocks=pretrained_config.num_blocks,
+                num_heads=pretrained_config.num_heads,
+                sequence_length=pretrained_config.sequence_length,
+                dropout=0.0,
+                batch_size=1,
+                epochs=1,
+                learning_rate=0.0,
+                patience=None,
+                min_delta=None,
+                device=torch.device("cpu"),
+            ),
+            vocab_size=pretrained_config.vocab_size,
+        )
+
+        pretrained_model = GPT2LMHeadModel.from_pretrained(pretrained_name)
+        pretrained_state = pretrained_model.state_dict()
+        model_state = model.state_dict()
+
+        model_state["embedding.weight"] = pretrained_state["transformer.wte.weight"]
+        model_state["positional_embedding.weight"] = pretrained_state[
+            "transformer.wpe.weight"
+        ]
+        model_state["layer_norm.weight"] = pretrained_state["transformer.ln_f.weight"]
+        model_state["layer_norm.bias"] = pretrained_state["transformer.ln_f.bias"]
+        model_state["linear.weight"] = pretrained_state["lm_head.weight"]
+
+        for i in range(pretrained_config.num_blocks):
+            # Attention block
+            model_state[f"blocks.{i}.heads.qkv_fc.weight"] = pretrained_state[
+                f"transformer.h.{i}.attn.c_attn.weight"
+            ].T
+            model_state[f"blocks.{i}.heads.qkv_fc.bias"] = pretrained_state[
+                f"transformer.h.{i}.attn.c_attn.bias"
+            ]
+            model_state[f"blocks.{i}.heads.projection.weight"] = pretrained_state[
+                f"transformer.h.{i}.attn.c_proj.weight"
+            ].T
+            model_state[f"blocks.{i}.heads.projection.bias"] = pretrained_state[
+                f"transformer.h.{i}.attn.c_proj.bias"
+            ]
+
+            # Norms
+            model_state[f"blocks.{i}.layer_norm1.weight"] = pretrained_state[
+                f"transformer.h.{i}.ln_1.weight"
+            ]
+            model_state[f"blocks.{i}.layer_norm1.bias"] = pretrained_state[
+                f"transformer.h.{i}.ln_1.bias"
+            ]
+            model_state[f"blocks.{i}.layer_norm2.weight"] = pretrained_state[
+                f"transformer.h.{i}.ln_2.weight"
+            ]
+            model_state[f"blocks.{i}.layer_norm2.bias"] = pretrained_state[
+                f"transformer.h.{i}.ln_2.bias"
+            ]
+
+            # FeedForward block
+            model_state[f"blocks.{i}.feed_forward.linear.weight"] = pretrained_state[
+                f"transformer.h.{i}.mlp.c_fc.weight"
+            ].T
+            model_state[f"blocks.{i}.feed_forward.linear.bias"] = pretrained_state[
+                f"transformer.h.{i}.mlp.c_fc.bias"
+            ]
+            model_state[f"blocks.{i}.feed_forward.projection.weight"] = (
+                pretrained_state[f"transformer.h.{i}.mlp.c_proj.weight"].T
+            )
+            model_state[f"blocks.{i}.feed_forward.projection.bias"] = pretrained_state[
+                f"transformer.h.{i}.mlp.c_proj.bias"
+            ]
+
+        model.load_state_dict(model_state)
+        return model
+
+    def forward(self, indices: torch.Tensor) -> torch.Tensor:
+        """
+        indices: [B, S]
+        """
+        E = self.embedding_size
+        V = self.vocab_size
+
+        B, S = indices.shape
+        assert_shape("indices", indices, (B, S))
+
+        # shape: [B, S, E]
+        tokens_embedding = self.embedding(indices)
+        assert_shape("tokens_embedding", tokens_embedding, (B, S, E))
+
+        # shape: [B, S]
+        positional_indices = self.positional_indices[:S].expand(B, -1)
+        assert_shape("positional_indices", positional_indices, (B, S))
+
+        # shape: [B, S, E]
+        positional_embedding = self.positional_embedding(positional_indices)
+        assert_shape("positional_embedding", positional_embedding, (B, S, E))
+
+        # shape: [B, S, E]
+        embedding = tokens_embedding + positional_embedding
+        assert_shape("embedding", embedding, (B, S, E))
+
+        # shape: [B, S, E]
+        for block in self.blocks:
+            embedding = block(embedding)
+            assert_shape("embedding", embedding, (B, S, E))
+
+        embedding = self.layer_norm(embedding)
+        assert_shape("embedding", embedding, (B, S, E))
+
+        # shape: [B, S, V]
+        output = self.linear(embedding)
+        assert_shape("output", output, (B, S, V))
+        return output
+
+    def generate(self, indices: torch.Tensor, max_length: int) -> torch.Tensor:
+        """
+        indices: [B, S] -- the batched sequence of indices
+        return: [B, S + max_length] -- after generated max_length tokens
+        """
+        self.eval()
+        V = self.vocab_size
+        for _ in range(max_length):
+            cropped_indices = indices[:, -self.sequence_length :]
+            B, S = cropped_indices.shape
+
+            # shape: [B, S, V]
+            output = self(cropped_indices)
+            assert_shape("output", output, (B, S, V))
+
+            # shape: [B, V]
+            last_output = output[:, -1, :]
+            assert_shape("last_output", last_output, (B, V))
+
+            # shape: [B, V]
+            probs = torch.softmax(last_output, dim=-1)
+            assert_shape("probs", probs, (B, V))
+
+            # shape: [B, 1]
+            next_token_indices = probs.multinomial(num_samples=1)
+            assert_shape("next_token_indices", next_token_indices, (B, 1))
+
+            # shape: [B, S + 1]
+            original_S = indices.shape[1]
+            indices = torch.cat([indices, next_token_indices], dim=-1)
+            assert_shape("indices", indices, (B, original_S + 1))
+
+        return indices
+
+
+@dataclass
+class Batch:
+    inputs: torch.Tensor
+    labels: torch.Tensor
+
+    @classmethod
+    def from_samples(cls, samples: list[Sample]) -> Self:
+        B = len(samples)
+        S = samples[0].input.shape[0]
+
+        # shape: [B, S] - Keep on CPU for multiprocessing compatibility
+        inputs = torch.stack([sample.input for sample in samples], dim=0)
+        assert_shape("inputs", inputs, (B, S))
+
+        # shape: [B, S] - Keep on CPU for multiprocessing compatibility
+        labels = torch.stack([sample.label for sample in samples], dim=0)
+        assert_shape("labels", labels, (B, S))
+
+        return cls(inputs=inputs, labels=labels)
+
+    def pin_memory(self) -> Self:
+        self.inputs.pin_memory()
+        self.labels.pin_memory()
+        return self
+
+
+class BatchLearner(Learner[Batch]):
+    def __init__(
+        self,
+        model: GPT2,
+        optimizer: optim.Optimizer,
+        criterion: nn.Module,
+        device: torch.device,
+    ):
+        super().__init__(model, optimizer, criterion, device)
+        self.model = model
+        assert self.criterion.reduction == "sum", "Reduction must be 'sum'"
+
+    def batch_step(self, batch: Batch) -> BatchResult:
+        B, S = batch.inputs.shape
+        V = self.model.vocab_size
+
+        # Move tensors to GPU in main process (not in workers)
+        inputs = batch.inputs.to(self.device, non_blocking=True)
+        labels = batch.labels.to(self.device, non_blocking=True)
+        assert_shape("inputs", inputs, (B, S))
+        assert_shape("labels", labels, (B, S))
+
+        # shape: [B, S, V]
+        outputs = self.model(inputs)
+        assert_shape("outputs", outputs, (B, S, V))
+
+        # `sum` mode
+        batch_loss = self.criterion(outputs.view(B * S, V), labels.view(B * S))
+
+        return BatchResult(
+            outputs=outputs,
+            labels=labels,
+            loss=batch_loss,
+            sample_count=B * S,
+        )
