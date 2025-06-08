46e8f59bb4eff289561788edd91724541873e50b
Hai Mai <thanhhaipmai@gmail.com>
2025-06-08 14:25:20 -0700

    Add another test that would find discrepancies between model vs pretrained_model

--- learning/src/learning/gpt2/model_test.py
+++ learning/src/learning/gpt2/model_test.py
@@ -22,7 +22,7 @@ def device() -> torch.device:
 
 
 @torch.no_grad()
-def test_model_load(tokenizer: tiktoken.Encoding, device: torch.device):
+def test_all_layers_output(tokenizer: tiktoken.Encoding, device: torch.device):
     model, pretrained_model = GPT2.from_pretrained(
         PretrainedName.GPT2_SMALL,
         device=torch.device("cpu"),
@@ -31,10 +31,78 @@ def test_model_load(tokenizer: tiktoken.Encoding, device: torch.device):
     model.eval()
     pretrained_model.eval()
 
-    print("-" * 80)
-    print(model)
-    print("-" * 80)
-    print(pretrained_model)
+    text = "Mary"
+    B = 1
+    S = 1
+    E = model.embedding_size
+
+    # Due to us not using FlashAttention, the outputs are not exactly the same.
+    atol = 1e-5
+    rtol = 1e-3
+
+    # ==== Verify Embedding ====
+    print("Validating embedding")
+
+    indices = torch.tensor([tokenizer.encode(text)])
+    assert_shape("indices", indices, (B, S))
+
+    tokens_embedding = model.embedding(indices)
+    assert_shape("tokens_embedding", tokens_embedding, (B, S, E))
+
+    positional_indices = model.positional_indices[:S].expand(B, -1)
+    positional_embedding = model.positional_embedding(positional_indices)
+    assert_shape("positional_embedding", positional_embedding, (B, S, E))
+
+    embedding = tokens_embedding + positional_embedding
+    assert_shape("embedding", embedding, (B, S, E))
+
+    hf_tokens_embedding = pretrained_model.transformer.wte(indices)
+    assert_shape("hf_tokens_embedding", hf_tokens_embedding, (B, S, E))
+
+    hf_positional_embedding = pretrained_model.transformer.wpe(
+        torch.arange(1).unsqueeze(0)
+    )
+    assert_shape("hf_positional_embedding", hf_positional_embedding, (B, S, E))
+
+    hf_embedding = hf_tokens_embedding + hf_positional_embedding
+    assert_shape("hf_embedding", hf_embedding, (B, S, E))
+
+    assert torch.allclose(embedding, hf_embedding, atol=atol, rtol=rtol)
+
+    # ==== Verify Blocks ====
+
+    output = embedding
+    hf_output = hf_embedding
+    for i in range(model.config.num_blocks):
+        print(f"Validating block {i}")
+        output = model.blocks_module[i](output)
+        hf_output = pretrained_model.transformer.h[i](hf_output)[0]
+        assert torch.allclose(output, hf_output, atol=atol, rtol=rtol)
+
+    # ==== Verify LayerNorm ====
+
+    print("Validating layer norm")
+    output = model.layer_norm(output)
+    hf_output = pretrained_model.transformer.ln_f(hf_output)
+    assert torch.allclose(output, hf_output, atol=atol, rtol=rtol)
+
+    # ==== Verify Logits ====
+
+    print("Validating logits")
+    logits = model.linear(output)
+    hf_logits = pretrained_model.lm_head(hf_output)
+    assert torch.allclose(logits, hf_logits, atol=atol, rtol=rtol)
+
+
+@torch.no_grad()
+def test_same_pretrained_logits(tokenizer: tiktoken.Encoding, device: torch.device):
+    model, pretrained_model = GPT2.from_pretrained(
+        PretrainedName.GPT2_SMALL,
+        device=torch.device("cpu"),
+    )
+
+    model.eval()
+    pretrained_model.eval()
 
     names = [
         "Mary",
